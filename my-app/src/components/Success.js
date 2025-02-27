4f0911ae644055d406e5ce270ff8ebccb13bd10c
zencane <zenrycane@gmail.com>
2025-02-27 02:10:55 +0000

    first version of app

---
+++ my-app/src/components/Success.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Success() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 p-4 text-center">
+      <h1 className="text-3xl font-bold mb-4">Success</h1>
+    </div>
+  );
+}
